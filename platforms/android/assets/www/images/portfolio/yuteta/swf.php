9a7d085bc11b10f8b244f1fad59f207347513a90
Ryu <tommy@slowalk.co.kr>
2016-09-14 20:09:14 +0900

    안드로이드 플랫폼 더해주기

---
+++ platforms/android/assets/www/images/portfolio/yuteta/swf.php
@@ -0,0 +1,9 @@
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+<embed src="yuteta.swf" width="1024" height="768"></embed>
